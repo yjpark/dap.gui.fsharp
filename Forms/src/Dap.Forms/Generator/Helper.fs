052260045a1c067c664af5ac0ef6b0b33693badd
YJ Park <yjpark@gmail.com>
2018-10-25 21:55:43 +0800

    cleanup feature generator

--- Forms/src/Dap.Forms/Generator/Helper.fs
+++ Forms/src/Dap.Forms/Generator/Helper.fs
@@ -10,23 +10,10 @@ open Dap.Gui
 open Dap.Gui.Generator
 open Dap.Forms.Generator.Gui
 
-let FeatureDapForms = "FEATURE_DAP_FORMS"
+let DapFormsFeature = "Dap.Forms.Feature"
 
 type G with
     static member Prefab<'widget when 'widget :> IWidget> (expr : Expr<'widget>) =
         G.Prefab (Gui, expr)
     static member PrefabFile<'widget when 'widget :> IWidget> (segments1 : string list, segments2 : string list, moduleName : string, expr : Expr<'widget>) =
         G.PrefabFile<'widget> (segments1, segments2, moduleName, Gui, expr)
-
-type G with
-    static member FormsAppPack (?switch : string) =
-        let switch = defaultArg switch FeatureDapForms
-        [
-            sprintf "#if %s" switch
-        ] @ G.AppPack (feature = "Dap.Forms.Feature")
-        @ [
-            sprintf "#else"
-        ] @ G.AppPack (?feature = None)
-        @ [
-            sprintf "#endif"
-        ]
