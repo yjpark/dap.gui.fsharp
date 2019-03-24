582af07add4a0f698854663d7b8e159a9c090b80
YJ Park <yjpark@gmail.com>
2019-03-24 20:57:59 +0800

    add link for known languages

--- src/Dap.Gui/Internal/Locale.fs
+++ src/Dap.Gui/Internal/Locale.fs
@@ -10,6 +10,9 @@ open Dap.Context
 open Dap.Platform
 open Dap.Gui
 
+// Full list of known language tags
+// https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/a9eac961-e77d-41a6-90a5-ce1a8b0cdb9c
+
 [<Literal>]
 let EN = "en"
 
