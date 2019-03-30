11a5ad078424a687e1cceee17b01ab7efbd81521
YJ Park <yjpark@gmail.com>
2019-03-30 15:26:54 +0800

    cleanup demo projects

--- demo/Demo.Mac/Program.fs
+++ demo/Demo.Mac/Program.fs
@@ -6,20 +6,12 @@ open Dap.Prelude
 open Dap.Context
 open Dap.Platform
 open Dap.Mac
-open Dap.Fabulous.Mac
 
 open Demo.App
 open Demo.Gui
-open Demo.Fabulous
-
-let useFabulous = true
 
 [<EntryPoint>]
 [<STAThread>]
 let main argv =
-    if useFabulous then
-        setFabulousMacParam <| MacParam.Create ("Demo")
-        App.RunFabulous ("demo-.log")
-    else
-        setMacParam <| MacParam.Create ("Demo")
-        App.RunGui ("demo-.log")
+    setMacParam <| MacParam.Create ("Demo")
+    App.RunGui ("demo-.log")
--- demo/Demo.iOS/Program.fs
+++ demo/Demo.iOS/Program.fs
@@ -6,20 +6,12 @@ open Dap.Prelude
 open Dap.Context
 open Dap.Platform
 open Dap.iOS
-open Dap.Fabulous.iOS
 
 open Demo.App
 open Demo.Gui
-open Demo.Fabulous
-
-let useFabulous = true
 
 [<EntryPoint>]
 [<STAThread>]
 let main argv =
-    if useFabulous then
-        setFabulousIOSParam <| IOSParam.Create ("Demo")
-        App.RunFabulous ("demo-.log")
-    else
-        setIOSParam <| IOSParam.Create ("Demo")
-        App.RunGui ("demo-.log")
+    setIOSParam <| IOSParam.Create ("Demo")
+    App.RunGui ("demo-.log")
